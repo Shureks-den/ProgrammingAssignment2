4befbc02ca685a0a4549b92a63760564903c4e17
Alex K <alexander.klonov@mail.ru>
2020-07-19 20:56:42 +0300

    Completed task with following functions: 1) makeCacheMatrix; 2)cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## First function 'makeCacheMatrix' create an list of 4 function:
+## 'set' assignes values of matrix to x
+## 'get' is there these values are contained
+## same principle for 'setsolve' and 'getsolve'
 
 makeCacheMatrix <- function(x = matrix()) {
+    n<-NULL
+    set<-function(y){
+        x<<-y
+        n<<-NULL
+    }
+    get<-function() x
+    setsolve<-function(solve) n<<-solve
+    getsolve<- function() n
+    list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## This function checks if 'getsolve' has inverted matrix in it, if not
+## this function will calculate it and write down.
+## It return inverted matrix of and x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    n<-x$getsolve()
+    if(!is.null(n)){
+        message("using cached data")
+        return(n)
+    }
+    data<-x$get()
+    n<-solve(data,...)
+    x$setsolve(n)
+    n
 }
